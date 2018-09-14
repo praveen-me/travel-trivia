997638b002e5db85abef5cb3e9d7e0932da5203e
praveen-me <praveenkrsaini0@gmail.com>
2018-09-14 10:23:32 +0530

    Set Initial Files

---
+++ js/quizup.js
@@ -0,0 +1,31 @@
+var questionArray = [
+  "What is the capital of Australia?",
+  "What is the capital of Liberia?",
+  "What is the capital of Taiwan?",
+  "What is the capital of Japan?",
+  "What is the capital of China?",
+  "What is the capital of Turkey?",
+  "What is the capital of Colombia?",
+  "What is the capital of India?"];
+
+
+var answerArray = [
+  ["Canberra", "Melbourne", "Sydney", "Darwin"],
+  ["Arthington","Monrovia","Tuzon","Marshall"],
+  ["Tainan City", "Taichung", "Taipei", "Hsinchu"],
+  ["Kyoto","Hiroshima","Tokyo","Osaka"],
+  ["Hong Kong", "Macau", "Shanghai", "Beijing"],
+  ["Ankara","Istanbul","Antalya","Bursa"],
+  ["Medellin", "Bogota", "Cartagena", "Cali"],
+  ["Mumbai","Hyderabad","Bangalore","New Delhi"]
+];
+
+var correctAnswers = [
+  "A. Canberra",
+  "B. Monrovia",
+  "C. Taipei",
+  "C. Tokyo",
+  "D. Beijing",
+  "A. Ankara",
+  "B. Bogota",
+  "D. New Delhi"];
